8398c56915feb6344262c77808b7f369c5ee9dd7
leejuno <zhqkfxm0127@naver.com>
2023-12-12 11:24:42 +0900

    feat: leejuno 스프링 프로젝트 파일 추가

---
+++ leejuno/src/main/java/com/example/leejuno/LeejunoApplication.java
@@ -0,0 +1,13 @@
+package com.example.leejuno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeejunoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeejunoApplication.class, args);
+	}
+
+}
